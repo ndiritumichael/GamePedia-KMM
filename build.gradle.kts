489e162754af6fafbd3557495072873b87ed59a1
ndiritumichael <ndiritumichael5@gmail.com>
2022-11-01 15:11:02 +0300

    convert root project gradle to use version catalogs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
 plugins {
     //trick: for the same plugin versions in all sub-modules
-    id("com.android.application").version("8.0.0-alpha06").apply(false)
-    id("com.android.library").version("8.0.0-alpha06").apply(false)
-    kotlin("android").version("1.7.10").apply(false)
-    kotlin("multiplatform").version("1.7.10").apply(false)
+
+
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.android.application) apply false
+
+    kotlin("android").version(libs.versions.kotlin.version).apply(false)
+    kotlin("multiplatform").version(libs.versions.kotlin.version).apply(false)
 }
 
 tasks.register("clean", Delete::class) {
